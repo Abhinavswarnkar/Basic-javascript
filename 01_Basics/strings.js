37a0c9a60c4b58b556616f30b0521d6ef0e4f4d1
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-06 05:03:55 +0000

    strings

---
+++ 01_Basics/strings.js
@@ -0,0 +1,11 @@
+const gameName = new String ('Abhinav');//invoke string as an object//
+// console.log(gameName);
+// console.log(gameName.length);
+// console.log(gameName[2]);
+// console.log(gameName.charAt(4));//prints the character at 4th index//
+// console.log(gameName.indexOf('i'));//prints the index no of i //
+
+const myName = new String ('palak-gec');
+console.log(myName);
+console.log(myName.substring(0,4));//prints the substring from 0 to 3 ,4th is not included//
+console.log(myName.slice(-9,3));//we can give -ve values at slice,it prints from reverse. 
