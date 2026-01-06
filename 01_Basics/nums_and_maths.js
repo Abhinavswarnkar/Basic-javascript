3482a6f8931ce39091b03fbfab53a0fe96e76339
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-06 12:56:08 +0000

    math

--- 01_Basics/nums_and_maths.js
+++ 01_Basics/nums_and_maths.js
@@ -1,7 +1,23 @@
 const num=100;
 console.log(num);
 const newNum= new Number(211.434);
-console.log(newNum);
-console.log(newNum.toString().length);
-console.log(newNum.toFixed(2));
-console.log(newNum.toPrecision(2));
+// console.log(newNum);
+// console.log(newNum.toString().length);//first it is converted to string then length can be checked
+// console.log(newNum.toFixed(2));//it gives fixed valued upto two places .
+// console.log(newNum.toPrecision(2));//this gives precised values till 2 places ,if decimal is there it will be rounded off.
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString());//It will convert the no two local string like 100,000.
+// //default will be as per US standard.
+// console.log(hundreds.toLocaleString('en-IN'));//this will give as per Indian standard(1,00,000)
+
+//*************************MATHS********************************** */
+
+// console.log(Math.abs(-5))//gives the absoute value only
+// console.log(Math.floor(3.12345))//gives the lowest value
+// console.log(Math.random());//gives random values b/w 0 to 1.
+// console.log(Math.random()*10);
+
+const max=20;
+const min=10;
+console.log(Math.floor((Math.random()*(max-min+1)+min)));
