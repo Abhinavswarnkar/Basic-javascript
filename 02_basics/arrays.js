65a92a9b6b0b8fe3baa31040dfa7521cb731020c
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-07 19:33:09 +0000

    array

---
+++ 02_basics/arrays.js
@@ -0,0 +1,20 @@
+const myArr= [1,2,3,4,5,6,7]; 
+// console.log(myArr);
+
+const myArr2 = new Array(7,5,4,3,2,1);
+// console.log(myArr2);
+// console.log(myArr2[2]);
+
+// myArr.push(8);// pushes elements at last in array.
+// console.log(myArr);
+// myArr.pop();// pops the last element in array.
+// console.log(myArr);
+
+myArr2.unshift(9);//adds element at the starting of array.
+ console.log(myArr2);
+myArr2.shift();//removes the starting element if array.
+console.log(myArr2);
+
+
+
+
