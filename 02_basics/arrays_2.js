da35c4ca0747c583a32ac6ee2e0724a4f7c08156
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-08 08:32:27 +0000

    arrays_2

--- 02_basics/arrays_2.js
+++ 02_basics/arrays_2.js
@@ -20,12 +20,21 @@ const newAllHeros=[...marvel_heros,...dc_heros];
 
 const anotherArray=[1,2,3,[4,5],6,[7,8,[9,6,7]]];
 const realAnotherArray=anotherArray.flat(Infinity);
-console.log(realAnotherArray);
+// console.log(realAnotherArray);
 
 //When there are nested arrays into each other , flat method is used to open all the depths
 //in the array and all the elements are stored in a new array .
 //we need to give the no of depths the flat could solve . we can give infinty for all the depths .
 
+
+console.log(Array.isArray('Hitesh'));//checks if the given parameter is array or not.
+console.log(Array.from('Hitesh'));//converts the given parameter to Array.Break down them , only iterable objects like strings.
+//Does not convert objects . we need to give key, value pairs.
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1,score2,score3));//creates new array from given arguments.
 
 
 
