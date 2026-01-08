f8f61351fc2366a4e44e3901671b0376223e78ab
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-08 04:43:22 +0000

    arrays_2

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -11,9 +11,31 @@ const myArr2 = new Array(7,5,4,3,2,1);
 // console.log(myArr);
 
 myArr2.unshift(9);//adds element at the starting of array.
- console.log(myArr2);
+//  console.log(myArr2);
 myArr2.shift();//removes the starting element if array.
-console.log(myArr2);
+// console.log(myArr2);
+
+// console.log(myArr.includes(9));//Gives the boolean value wheather the element exist in array or not. 
+// console.log(myArr.indexOf(4));//Gives the index of element.
+// console.log(myArr.indexOf(10));//If the element is not there in array, it will print index as -1.
+
+//*********************SLICE & SPLICE******************************************
+
+const nyArray = [1,2,3,4,5,6];
+console.log('A',nyArray);
+console.log(nyArray.slice(1,3));//slice the array into parts. 3rd element will not be included. 
+console.log('B',nyArray);
+console.log(nyArray.splice(1,3));//This also split the array into parts but in these 3rd element will also be included.
+console.log(nyArray);
+
+/* key differece between slice and splice is that after using slice the original array 
+remains the same , it is not manipulated , but after using Splice the original array is
+manipulated. The elements which are spliced gets remove from the original array.*/
+
+
+
+
+
 
 
 
---
+++ 02_basics/arrays_2.js
@@ -0,0 +1,31 @@
+const marvel_heros=['thor','ironman','spiderman'];
+const dc_heros=['superman','flash','batman'];
+
+//marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+//whole dc_heros array is pushed into marvel_heros array as one element, which is not a good practice.
+//thats why we dont perfer to push one array into another .
+//any new array is not formed in the case of push method.
+
+const all_Heros=marvel_heros.concat(dc_heros);
+// console.log(all_Heros);
+//concats creates a new array and pushes all the elements of both the array into a new one .
+// It is nore effective then push.
+
+//*************spread method**********
+const newAllHeros=[...marvel_heros,...dc_heros];
+// console.log(newAllHeros);
+//Best snd most widely used method to join two arrays.
+//creates a new array and pushes the elemets of both the array to it.
+
+const anotherArray=[1,2,3,[4,5],6,[7,8,[9,6,7]]];
+const realAnotherArray=anotherArray.flat(Infinity);
+console.log(realAnotherArray);
+
+//When there are nested arrays into each other , flat method is used to open all the depths
+//in the array and all the elements are stored in a new array .
+//we need to give the no of depths the flat could solve . we can give infinty for all the depths .
+
+
+
+
