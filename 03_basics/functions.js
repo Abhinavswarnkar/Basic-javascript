00187d1efdb8683aa0743647883db258069003ab
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-16 04:01:23 +0000

    obj and arrays in function

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -16,20 +16,56 @@ function addTwoNo(number1,number2){
 }
 
 const result =addTwoNo(3,4);
-console.log(result);
+// console.log(result);
 
 function userLogin(username){
     if(!username){
-        console.log("please enter username");
+        // console.log("please enter username");
         return;
     }
     return `${username} successfully logged in`
 }
 let name= userLogin()
-console.log(name);
+// console.log(name);
 
+// *****************REST Operator*****************************
 
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,300,400));
+//... is the Rest operator which packs every argument in a bundle and returns it as an array.
 
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,300,400,500));
+//Takes first two values as val1, val2 and return rest values in an array as REST operator is used.
 
+// *********************Object in a function*****************************
+const user={
+    username:"Abhinav",
+    price:299
+} 
 
+function handleObject(anyObject){
+        return `${anyObject.username} is the username and ${anyObject.price} is the price`
+ }
 
+//  console.log(handleObject(user))
+// we can also declare object properties while calling function .
+// // console.log(handleObject({
+//     username:"abhinav",
+//     // price:299
+// // })) 
+
+// **********************Arrays in function**************
+
+const myArray=[100,200,300]
+
+function returnsSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnsSecondValue(myArray));
+// we can also declare arguments during calling a function.
+console.log(returnsSecondValue([100,300,200,400]));
