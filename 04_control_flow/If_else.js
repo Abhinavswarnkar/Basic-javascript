b071507d0530cf5ffbc8d97456c20958e5a5c799
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-19 11:12:09 +0000

    if else and switch

--- 04_control_flow/If_else.js
+++ 04_control_flow/If_else.js
@@ -1,14 +1,17 @@
-let temperature=26;
-if(temperature>35){
-    console.log("temp is above 35");
+// let temperature=26;
+// if(temperature>35){
+//     console.log("temp is above 35");
     
-} else if (temperature<35 && temperature>25)
-    {
-     console.log("temp is below 35");
+// } else if (temperature<35 && temperature>25)
+//     {
+//      console.log("temp is below 35");
      
-}else{
-console.log("temperature is below room temp");
+// }else{
+// console.log("temperature is below room temp");
 
-}
+// }
 //This is classic nested if else example .
 // we can use && (AND) operator and || (OR) operator to give the conditions.
+ let balance=50;
+ if(balance>=50)console.log('balance is greater than 50'), console.log('balance is less than 50');
+ // we can write if condition like this also. but it is not a good practice.
--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,9 +1,14 @@
-let month = "jan";
+let month = "feb";
 switch (month) {
-    case feb:
+    case "feb":
+        console.log("month is feb");
         
         break;
 
+        case "jan":
+            console.log('month is jan ')
+        break;
     default:
+        console.log("month is not given")
         break;
 }
