b2a8341d35532d51121190affd2f8e5bad38e737
Abhinavswarnkar <abhinavswarnkar2018@gmail.com>
2026-01-20 13:36:00 +0000

    do-while

---
+++ 05_iteration/do_while.js
@@ -0,0 +1,21 @@
+// let i = 0
+// while (i<=10) {
+//     console.log(`value is ${i}`);
+//     i=i+2
+// }
+
+// let myArray=["flash","superman","batman"]
+// let arr=0
+// while (arr<myArray.length) {
+//     console.log(`value is ${myArray[arr]}`)
+//     arr=arr+1 
+// }
+
+let j=1
+
+do {console.log(`value is ${j}`);
+j=j+2
+    
+} while (j<=10);
+//do-while loop checks the condition at last . 
+// therefore if you want to print one value u can .
